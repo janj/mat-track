25d25b04dfae81d51d849357aed49b24b6ece036
Jan <jan.jirout@indiegogo.com>
2017-07-19 22:56:16 -0700

    refacctor, added upper

--- guts.js
+++ guts.js
@@ -1,29 +1,38 @@
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-const buildDisplay = (elementId, sortedSteps) => {
-	const stepData = stepDataFactory(sortedSteps);
-	const runner = stepRunner(stepData);
-	styleInit();
-	let rootNode = document.getElementById(elementId);
-	rootNode.innerHTML = "";
-	rootNode.appendChild(buildHeader(runner));
-	rootNode.appendChild(matTableElement(stepData));
-	rootNode.appendChild(buildFooter());
-}
+(function styleInit() {
+    var style_rules = [];
+    style_rules.push("td { width: 12px; height: 12px; }");
+	style_rules.push("td.off { background-color: lightgray; }")
+	style_rules.push("td.never { background-color: gray; }")
+	style_rules.push("td.on { background-color: magenta; }")
+    style_rules.push("table { border-collapse:collapse; margin: 20px auto 20px auto; }");
+	style_rules.push(".footer span { padding: 5px; }")
+    const style = document.createElement("style");
+    style.type = "text/css";
+    style.innerHTML = style_rules.join("\n");
+    document.head.appendChild(style);
+})();
 
-const cellForStep = (step) => {
-	return document.getElementById(cellId(step.X, step.Y));
+const buildDisplay = (displays) => {
+	_.each(displays, (display) => {
+		const stepData = stepDataFactory(display.steps);
+		const runner = stepRunner(stepData);
+		const displayController = displayFactory(display.id, runner, stepData);
+		runner.setDisplay(displayController);
+	});
 }
 
 const stepDataFactory = (steps) => {
-	const xUniq = _.uniq(_.map(sorted, (s)=> s.X))
-	const yUniq = _.uniq(_.map(sorted, (s)=> s.Y))
+	const xUniq = _.uniq(_.map(steps, (s)=> s.X))
+	const yUniq = _.uniq(_.map(steps, (s)=> s.Y))
 
 	const ROOM_WIDTH = _.max(xUniq);
 	const ROOM_HEIGHT = _.max(yUniq);
 
 	const pairKey = (x, y) => `${x},${y}`;
 	const pairs = {};
-	_.each(sorted, (s) => { pairs[pairKey(s.X, s.Y)] = true; })
+	_.each(steps, (s) => { pairs[pairKey(s.X, s.Y)] = true; })
 
 	return {
 		stepAtIndex: (index) => steps[index],
@@ -38,43 +47,41 @@ const stepRunner = (stepData) => {
 	let currentStepIndex = -1;
 	let running = false;
 	let pause = 75;
+	let displayController = null;
 	
 	const currentStep = () => stepData.stepAtIndex(currentStepIndex);
+	const atEnd = () => currentStepIndex >= stepData.numberOfSteps;
+	const updateDisplay = () => displayController.updateDisplay(currentStep(), currentStepIndex);
 
 	const setClassForStepAtIndex = (stepIndex, elementClass) => {
 		const step = stepData.stepAtIndex(stepIndex);
 		cellForStep(step).className = elementClass;
 	}
 	
-	const atEnd = () => currentStepIndex >= stepData.numberOfSteps;
-
-	const updateDisplay = () => {
-		const step = currentStep();
-		updateTextElement("stepIndex", `Step: ${currentStepIndex}`)
-		updateTextElement("timestamp", `At: ${step.Timestamp}`);
-		updateTextElement("coord", `X: ${step.X}, Y: ${step.Y}, Z: ${step.Z}`);
+	const updateCurrentStep = (isOn) => {
+		displayController.updateCell(currentStep(), isOn);
 	}
 
 	const step = () => {
 		if (atEnd()) return;
 		if (currentStepIndex >= 0) {
-			setClassForStepAtIndex(currentStepIndex, "off");
+			updateCurrentStep(false);
 		}
 		currentStepIndex++;
-		setClassForStepAtIndex(currentStepIndex, "on");
+		updateCurrentStep(true);
 		updateDisplay();
 	}
 
 	const back = () => {
 		if (currentStepIndex <= 0) return;
-		setClassForStepAtIndex(currentStepIndex, "off");
+		updateCurrentStep(false);
 		currentStepIndex--;
-		setClassForStepAtIndex(currentStepIndex, "on");
+		updateCurrentStep(true);
 		updateDisplay();
 	}
 	
-	const slower = () => pause += 25;
-	const faster = () => pause = _.max(0, pause - 25);
+	const slower = () => { pause += 25 };
+	const faster = () => { pause = Math.max(0, pause - 25) };
 	
 	const runLoop = (restart) => {
 		if (restart) {
@@ -89,13 +96,86 @@ const stepRunner = (stepData) => {
 	return {
 		step, back, faster, slower,
 		pause: () => { running = false; },
-		doNext: () => { runLoop(true); }
+		doNext: () => { runLoop(true); },
+		setDisplay: (display) => displayController = display
 	};
 }
 
-const updateTextElement = (elementId, text) => {
-	const element = document.getElementById(elementId);
-	element.innerHTML = text;
+const displayFactory = (key, runner, stepData) => {
+	const idx = (elementId) => `${key}_${elementId}`;
+	
+	const updateTextElement = (elementId, text) => {
+		const element = document.getElementById(idx(elementId));
+		element.innerHTML = text;
+	}
+	
+	const buildButton = (text, fnct) => {
+		var btn = document.createElement('button');
+		btn.className = 'btn btn-default';
+		btn.appendChild(document.createTextNode(text));
+		btn.onclick = fnct;
+		return btn;
+	}
+
+	const buildFooter = () => {
+		const footer = document.createElement("div");
+		footer.className = "footer";
+		_.each(["stepIndex", "timestamp", "coord"], (spanId) => {
+			const spanElement = document.createElement("span");
+			spanElement.id = idx(spanId);
+			footer.appendChild(spanElement);
+		});
+		return footer;
+	}
+	
+	const cellId = (x, y) => idx(`c.${x}.${y}`);
+	
+	const matTableElement = (stepData) => {
+		let matTable = document.createElement("table");
+		for(let y=stepData.height; y>=0; y--) {
+			let rowNode = document.createElement("tr");
+			for(let x=0; x<=stepData.width; x++) {
+				let cell = document.createElement("td");
+				cell.id = cellId(x, y);
+				cell.className = stepData.isSteppedOn(x, y) ? "off" : "never";
+				rowNode.appendChild(cell);
+			}
+			matTable.appendChild(rowNode);
+		}
+		return matTable;
+	}
+	
+	const updateDisplay = (step, currentStepIndex) => {
+		updateTextElement("stepIndex", `Step: ${currentStepIndex}`);
+		updateTextElement("timestamp", `At: ${step.Timestamp}`);
+		updateTextElement("coord", `X: ${step.X}, Y: ${step.Y}, Z: ${step.Z}`);
+	}
+	
+	const updateCell = (step, isOn) => {
+		const cell = document.getElementById(cellId(step.X, step.Y));
+		cell.className = isOn ? "on" : "off";
+	}
+	
+	const buildHeader = (runner) => {
+		let = header = document.createElement("div");
+		header.appendChild(buildButton("Next Steps", runner.doNext));
+		header.appendChild(buildButton("Pause", runner.pause));
+		header.appendChild(buildButton("Step", runner.step));
+		header.appendChild(buildButton("Back", runner.back));
+		header.appendChild(buildButton("Faster", runner.faster));
+		header.appendChild(buildButton("Slower", runner.slower));
+		return header;
+	};
+	
+	let rootNode = document.getElementById(key);
+	rootNode.innerHTML = "";
+	rootNode.appendChild(buildHeader(runner));
+	rootNode.appendChild(matTableElement(stepData));
+	rootNode.appendChild(buildFooter());
+	
+	return {
+		updateDisplay, updateCell
+	};
 }
 
 const download = (text, name, type) => {
@@ -104,72 +184,4 @@ const download = (text, name, type) => {
     a.href = URL.createObjectURL(file);
     a.download = name;
     a.click();
-}
-
-const buildButton = (text, fnct) => {
-	var btn = document.createElement('button');
-	btn.id = text;
-	btn.className = 'btn btn-default';
-	btn.appendChild(document.createTextNode(text));
-	btn.onclick = fnct;
-	return btn;
-}
-
-const buildHeader = (runner) => {
-	let = header = document.createElement("div");
-	header.appendChild(buildButton("Next Steps", runner.doNext));
-	header.appendChild(buildButton("Pause", runner.pause));
-	header.appendChild(buildButton("Step", runner.step));
-	header.appendChild(buildButton("Back", runner.back));
-	header.appendChild(buildButton("Faster", runner.faster));
-	header.appendChild(buildButton("Slower", runner.slower));
-	return header;
-}
-
-const buildFooter = () => {
-	const footer = document.createElement("div");
-	footer.id = "footer";
-	
-	_.each(["stepIndex", "timestamp", "coord"], (spanId) => {
-		const spanElement = document.createElement("span");
-		spanElement.id = spanId;
-		footer.appendChild(spanElement);
-	});
-
-	return footer;
-}
-
-const cellId = (x, y) => "c."+x+"."+y;
-
-const matTableElement = (stepData) => {
-	let matTable = document.createElement("table");
-	for(let y=stepData.height-1; y>=0; y--) {
-		let rowNode = document.createElement("tr");
-		for(let x=0; x<=stepData.width; x++) {
-			let cell = document.createElement("td");
-			cell.id = cellId(x, y);
-			cell.className = stepData.isSteppedOn(x, y) ? "off" : "never";
-			rowNode.appendChild(cell);
-		}
-		matTable.appendChild(rowNode);
-	}
-	return matTable;
-}
-
-let styleInit = () => {
-    var style_rules = [];
-    style_rules.push("td { width: 12px; height: 12px; }");
-	style_rules.push("td.off { background-color: lightgray; }")
-	style_rules.push("td.never { background-color: gray; }")
-	style_rules.push("td.on { background-color: magenta; }")
-    style_rules.push("table { border-collapse:collapse; margin: 20px auto 20px auto; }");
-	style_rules.push("#footer span { padding: 5px; }")
-    var style = document.createElement("style");
-    style.type = "text/css";
-    style.innerHTML = style_rules.join("\n");
-    document.head.appendChild(style);
-}
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
